76d1b0ab2e7c71af9c4fc97db39436b9ac30f753
Angel Velasquez <pcisavel@upc.edu.pe>
2026-09-21 11:40:11 -0500

    feat(layout): add Main View component JavaScript elements

---
+++ src/shared/presentation/components/layout.vue
@@ -0,0 +1,21 @@
+<script setup>
+  import {useI18n} from "vue-i18n";
+  import {ref} from "vue";
+
+  const { t } = useI18n();
+  const drawer = ref(false);
+  const toggleDrawer = () => { drawer.value = !drawer.value; }
+
+  const items = [
+    { label: 'option.home', to: '/home'},
+    { label: 'option.about', to: '/about'}
+  ];
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
