b2d18474f1227aa49d332192264227f889bb660e
Angel Velasquez <pcisavel@upc.edu.pe>
2026-09-21 12:06:06 -0500

    feat(in-app-navigation): configure routes for home and about views

--- src/router.js
+++ src/router.js
@@ -1,12 +1,22 @@
 import {createRouter, createWebHistory} from "vue-router";
+import Home from "./shared/presentation/views/home.vue";
+import About from "./shared/presentation/views/about.vue";
 
 const routes = [
+    { path: '/home',    name: 'home',   component: Home,    meta: { title: 'Home'}},
+    { path: '/about',   name: 'about',  component: About,   meta: { title: 'About'}}
 
 ];
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: routes
+});
+
+router.beforeEach((to, from) => {
+    let baseTitle = 'ACME Learning Center';
+    document.title = `${baseTitle} - ${to.meta.title}`;
+    return true;
 });
 
 export default router;
