b2ea1d8a9b5d24b2cfb7b174670a5fbf35d5546f
Angel Velasquez <pcisavel@upc.edu.pe>
2026-09-21 12:25:27 -0500

    feat(page-not-found): add Page Not Found view

--- src/router.js
+++ src/router.js
@@ -1,8 +1,13 @@
 import {createRouter, createWebHistory} from "vue-router";
+import Home from "./shared/presentation/views/home.vue";
+import About from "./shared/presentation/views/about.vue";
+import PageNotFound from "./shared/presentation/views/page-not-found.vue";
 
 const routes = [
-    { path: '/home',    name: 'home',   component: Home,    meta: { title: 'Home'}},
-    { path: '/about',   name: 'about',  component: About,   meta: { title: 'About'}}
+    { path: '/home',            name: 'home',       component: Home,            meta: { title: 'Home'}},
+    { path: '/about',           name: 'about',      component: About,           meta: { title: 'About'}},
+    { path: '/',                redirect: '/home'},
+    { path: '/:pathMatch(.*)*', name: 'not-found',  component: PageNotFound,    meta: { title: 'Page Not Found'}}
 ];
 
 const router = createRouter({
